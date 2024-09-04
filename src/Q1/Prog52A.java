340931b30e138a4ba0853ad8ccdf7a19b565cd88
AydenPhillips07 <aydenphillips1119@gmail.com>
2024-09-04 11:17:38 -0500

    Hello! 9/4/24

---
+++ src/Q1/Prog52A.java
@@ -0,0 +1,18 @@
+package Q1;
+
+public class Prog52A {
+    public static void main(String[] args) {
+        // Java types : int, double, boolean, and String
+        int length = 143;
+        int width = 80;
+        width = 82;
+
+        // + - * / %
+        int area = length * width;
+        int perim = 2 * length + 2 * width;
+
+
+
+
+   }
+}
---
+++ src/Q1/Prog54B.java
@@ -0,0 +1,10 @@
+package Q1;
+import java.util.*;
+
+public class Prog54B {
+    public static void main(String[] args) {
+
+        System.out.print("Enter num1: ");
+
+    }
+}
