1e2e4d89b2862a6e0e2f9c1a9c4cdff64a5599bc
AydenPhillips07 <aydenphillips1119@gmail.com>
2024-09-19 11:24:11 -0500

    Update 9/19/2024

---
+++ src/Q1/Prog72t.java
@@ -0,0 +1,29 @@
+package Q1;
+
+import java.util.*;
+
+public class Prog72t {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first time: ");
+        int time1 = input.nextInt();
+        System.out.println();
+
+
+        System.out.print("Enter the second time: ");
+        int time2 = input.nextInt();
+        System.out.println();
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
