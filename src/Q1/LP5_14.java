2829c44b38e1792f0c1e8de11df76542f463a95b
AydenPhillips07 <aydenphillips1119@gmail.com>
2024-10-30 11:25:11 -0500

    Update 10/29/2024

---
+++ src/Q1/LP5_14.java
@@ -0,0 +1,11 @@
+package Q1;
+
+import java.util.Random;
+
+public class LP5_14 {
+    public static void main(String[] args) {
+
+            int numo = (int) (Math.random() * (6 - 1) + 1);
+            int numi = (int) (Math.random() * (6 - 1) + 1);
+    }
+}
