de6a610c1db83c38d9480bafdbc4ab8ab222eb72
AydenPhillips07 <aydenphillips1119@gmail.com>
2024-11-15 11:23:58 -0600

    Update 11/15/2024

---
+++ src/Q2/Prog435a/CL435a.java
@@ -0,0 +1,21 @@
+package Q2.Prog435a;
+
+public class CL435a {
+    private int vehicle;
+    private int toll;
+    private double basetoll;
+    private double factor;
+    private double cost;
+
+    public CL435a(int car, int gate) {
+        vehicle = car;
+        toll = gate;
+        basetoll = 0.00;
+        factor = 0.00;
+        cost = 0.00;
+    }
+
+
+
+
+}
