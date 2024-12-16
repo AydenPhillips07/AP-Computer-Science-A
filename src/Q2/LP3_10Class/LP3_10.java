675af9499acfee1e5e53688cf3dc0de88c73a3a7
AydenPhillips07 <aydenphillips1119@gmail.com>
2024-12-16 11:21:28 -0600

    Update 12/16/2024

--- src/Q2/LP3_10Class/LP3_10.java
+++ src/Q2/LP3_10Class/LP3_10.java
@@ -1,7 +1,17 @@
 package Q2.LP3_10Class;
 
+import java.util.Scanner;
+
 public class LP3_10 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of burgers: ");
+        int burgers = input.nextInt();
+        System.out.println("Enter the number of fries: ");
+        int fries = input.nextInt();
+        System.out.println("Enter the number of sodas: ");
+        int sodas = input.nextInt();
+        System.out.println("Enter amount tendered: ");
+        double amount = input.nextDouble();
     }
 }
--- src/Q2/LP3_10Class/lp3_10class.java
+++ src/Q2/LP3_10Class/lp3_10class.java
@@ -6,15 +6,30 @@ public class lp3_10class {
     private int sodas;
     private double btax;
     private double tax;
+    private double amtgiv;
     private double total;
+    private double change;
 
 
-    public lp3_10class(int burg, int fri, int sod){
+    public lp3_10class(int burg, int fri, int sod, double amount){
         burgers = burg;
         fries = fri;
         sodas = sod;
         btax = 0.00;
         tax = 0.00;
+        amtgiv = amount;
         total = 0.00;
+        change = 0.00;
+
+    }
+    public void calc(){
+        burgers *= 1.69;
+        fries *= 1.09;
+        sodas *= 0.99;
+        btax = burgers + fries + sodas;
+        tax = btax * 0.65;
+        total = btax + tax;
+        change = amtgiv - total;
+
     }
 }
