1c347fa2d1d3194f47033a1446724063589656bb
AydenPhillips07 <143823536+AydenPhillips07@users.noreply.github.com>
2025-02-07 15:09:52 -0600

    Update Prog477a.java

--- src/Q3/Prog477a.java
+++ src/Q3/Prog477a.java
@@ -7,14 +7,24 @@ import java.util.Scanner;
 public class Prog477a {
     public static void main(String[] args) {
         try {
-            Scanner file = new Scanner(new File("Langdat/FILENAME"));
+            Scanner file = new Scanner(new File("Langdat/prog477a.dat"));
             int[][] mat = new int[5][4];
+            int maltot = 0;
+            int femtot = 0;
+            int Atot = 0;
+            int Btot = 0;
+            int Ctot = 0;
+            int Dtot = 0;
+            int Ftot = 0;
 
             while (file.hasNext()) {
                 int gender = file.nextInt();
                 int grade = file.nextInt();
                 file.nextLine();
             }
+
+            if (gender == 1)
+                
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
