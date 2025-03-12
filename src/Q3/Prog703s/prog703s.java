89d9d0fdccc11785df8e8b3d3a53a8f10f588b99
AydenPhillips07 <aydenphillips1119@gmail.com>
2025-03-12 11:11:12 -0500

    Update 3/12/2025

--- src/Q3/Prog703s/prog703s.java
+++ src/Q3/Prog703s/prog703s.java
@@ -10,22 +10,19 @@ public class prog703s {
         try {
             Scanner file = new Scanner(new File("Langdat/prog703s.txt"));
             ArrayList<Computers> computer = new ArrayList<>();
-                int num = file.nextInt();
+            int num = file.nextInt();
+            while(file.hasNext()) {
                 if (num == 1) {
                     String name = file.next();
                     int numb = file.nextInt();
                     double value = file.nextDouble();
                     String col = file.next();
-
-
-
-
-                } else if (num == 2) {
+                    System.out.println(col);
+                }else if (num == 2) {
                     String name = file.next();
                     int numb = file.nextInt();
                     double value = file.nextDouble();
                     double ver = file.nextDouble();
-
 
                 } else if (num == 3) {
                     String name = file.next();
@@ -33,6 +30,7 @@ public class prog703s {
                     double value = file.nextDouble();
                     int cod = file.nextInt();
 
+                    }
                 }
 
 
