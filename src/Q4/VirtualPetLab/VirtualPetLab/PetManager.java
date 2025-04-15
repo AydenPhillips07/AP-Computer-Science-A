2c972e767adc9dc54076b0fd3ecb534cb400c17e
AydenPhillips07 <aydenphillips1119@gmail.com>
2025-04-15 11:23:41 -0500

    4/15/2025

--- src/Q4/VirtualPetLab/VirtualPetLab/PetManager.java
+++ src/Q4/VirtualPetLab/VirtualPetLab/PetManager.java
@@ -5,6 +5,16 @@ public class PetManager {
     private ArrayList<Pet> pets = new ArrayList<>();
 
     // TODO: Implement methods as needed to interact with the pets list
-    public void addPet(Pet pet) {}
+    public void addPet(Pet pet) {
+        pets.add(pet);
+    }
+
+    public void removePet(Pet pet) {
+        pets.remove(pet);
+    }
+
+    public ArrayList<Pet> getList(Pet pet) {
+        return pets;
+    }
 
 }
