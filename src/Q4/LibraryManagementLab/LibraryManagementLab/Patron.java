36dc611d5ca71b5809e84e0231a5a2f845fe8e33
AydenPhillips07 <aydenphillips1119@gmail.com>
2025-04-30 11:23:07 -0500

    4/30/2025

--- src/Q4/LibraryManagementLab/LibraryManagementLab/Patron.java
+++ src/Q4/LibraryManagementLab/LibraryManagementLab/Patron.java
@@ -17,6 +17,16 @@ public class Patron {
     public String getName() { return name; }
     public String getPatronId() { return patronId; }
 
+
+
     // TODO: Implement checkInBook(Book book) and checkOutBook(Book book) methods (remove/add book from checkedOutBooks)
     // Don't forget to set the book's checkedOut field to true/false
+    public void checkInBook(Book book){
+        checkedOutBooks.remove(book);
+        book.setCheckedOut(false);
+    }
+    public void checkOutBook(Book book){
+        checkedOutBooks.add(book);
+        book.setCheckedOut(true);
+    }
 }
