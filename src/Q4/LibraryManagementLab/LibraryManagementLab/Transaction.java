d73e765e5310f2c02cf74787dbc7c334ca608bfc
AydenPhillips07 <aydenphillips1119@gmail.com>
2025-05-08 11:23:15 -0500

    5/8/2025

--- src/Q4/LibraryManagementLab/LibraryManagementLab/Transaction.java
+++ src/Q4/LibraryManagementLab/LibraryManagementLab/Transaction.java
@@ -1,22 +1,28 @@
 package Q4.LibraryManagementLab.LibraryManagementLab;
 
 public class Transaction {
-    private String isbn;
-    private String patronId;
-    private String checkoutDate; // Simple date format
-    private String returnDate; // Optional, can be null if not returned yet
+    private String myIsbn;
+    private String myPatronId;
+    private String myCheckoutDate; // Simple date format
+    private String myReturnDate; // Optional, can be null if not returned yet
 
     // TODO: Implement constructor (isbn, patronId, checkoutDate); set returnDate to null by default
+    public Transaction(String isbn, String patronId, String checkoutDate){
+        myIsbn = isbn;
+        myPatronId = patronId;
+        myCheckoutDate = checkoutDate;
+        myReturnDate = null;
+    }
 
     // TODO: Implement getters and setters
 
     @Override
     public String toString() {
         return "Transaction{" +
-                "isbn='" + isbn + '\'' +
-                ", patronId='" + patronId + '\'' +
-                ", checkoutDate='" + checkoutDate + '\'' +
-                ", returnDate='" + returnDate + '\'' +
+                "isbn='" + myIsbn + '\'' +
+                ", patronId='" + myPatronId + '\'' +
+                ", checkoutDate='" + myCheckoutDate + '\'' +
+                ", returnDate='" + myReturnDate + '\'' +
                 '}';
     }
 }
